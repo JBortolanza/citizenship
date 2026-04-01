b73682d77f2a64a8c5e5dbee68ab22406ff3144f
JBortolanza <jan.bortolanza@gmail.com>
2026-04-01 19:20:30 -0300

    finished config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [] = useState(0);
 
   return (
     <>
-      <h1>hello world test again</h1>
+      <h1>hello world test </h1>
     </>
   );
 }
